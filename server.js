479c891bb12445c1bfb706fba55435f392961f4e
Andre Rorvik <andrero@uia.no>
2025-05-08 13:01:19 +0200

    added some rest tests and returned stringified data

--- server.js
+++ server.js
@@ -15,15 +15,17 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-    console.log('Yay. I hit an endpoint', req.method)
-    res.send('<h1>Homepage</h1>')
+    res.send(`<body style="background-color:lightblue">
+                <h1>Data:</h1>
+                <p style="color:green">${JSON.stringify(data)}</p>
+              </body>`)
 })
 
 app.get('/dashboard', (req, res) => {
     res.send('<h1>Dashboard</h1>')
 })
 
-app.get('api/data', (req, res) => {
+app.get('/api/data', (req, res) => {
     console.log('This one is for data')
     res.send(data)
 })
